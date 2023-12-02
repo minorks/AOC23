2546e1935afc48d7400a1b02fd91b1c7e1684698
Kyle Minor <minorks@vt.edu>
2023-12-02 14:04:15 -0500

    initial commit

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 13:13:18 2023
+
+@author: hokie
+"""
+
---
+++ testing.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 13:10:49 2023
+
+@author: hokie
+"""
+
+print("Hello World")
